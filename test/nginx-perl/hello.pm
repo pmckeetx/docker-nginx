98909a2cf7a1bd9b1b1d4d1340193a44d7bd11d6
David Dulek <ddulek@redhat.com>
2021-05-07 13:52:06 -0500

    testing perl module in nginx

---
+++ test/nginx-perl/hello.pm
@@ -0,0 +1,21 @@
+package hello;
+
+use nginx;
+
+sub handler {
+    my $r = shift;
+
+    $r->send_http_header("text/html");
+    return OK if $r->header_only;
+
+    $r->print("hello!\n<br/>");
+
+    if (-f $r->filename or -d _) {
+        $r->print($r->uri, " exists!\n");
+    }
+
+    return OK;
+}
+
+1;
+__END__
