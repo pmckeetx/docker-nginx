f275d0393ffe3c19fb91045e9e671573d9d5f9ff
David Dulek <ddulek@redhat.com>
2021-05-13 11:11:54 -0500

    sending file if a file exists2

--- test/nginx-perl/hello.pm
+++ test/nginx-perl/hello.pm
@@ -5,13 +5,15 @@ use nginx;
 sub handler {
     my $r = shift;
 
-    $r->send_http_header("text/html");
+#    $r->send_http_header("text/html");
     return OK if $r->header_only;
 
-    $r->print("hello!\n<br/>");
+#    $r->print("hello!\n<br/>");
 
     if (-f $r->filename) {
+        $r->send_http_header;
         $r->sendfile($r->uri);
+        $r->flush();
     }
     if (-d $r->filename) {
         $r->print($r->uri, " exists!\n");
