7d2ea23663aeafd28be49164dc96b7d3caaa8533
David Dulek <ddulek@redhat.com>
2021-05-14 15:47:52 -0500

    Getting data from the S3 bucket

--- test/nginx-perl/hello.pm
+++ test/nginx-perl/hello.pm
@@ -22,10 +22,12 @@ sub handler {
         return OK;
     }
 
+    my $filename = $r->uri;
+	my $bucketname = $s3->bucket($bucket->bucket);
+    $filename =~ s/^\///;
+
     if (-f $r->filename) {
         $r->send_http_header;
-        my $filename = $r->uri;
-        $filename =~ s/^\///;
         $r->sendfile($filename);
         $r->flush();
     }
@@ -33,24 +35,6 @@ sub handler {
     if (-d $r->filename) {
         $r->send_http_header("text/html");
         $r->print($r->uri, " exists!<br>\n");
-        my $response = $s3->buckets;
-        $r->print($s3->buckets, "buckets.<br>\n");
-	my $bucketname;
-	foreach my $bucket ( @{ $response->{buckets} } ) {
-	    print "You have a bucket: " . $bucket->bucket . "\n";
-	    $bucketname = $s3->bucket($bucket->bucket);
-	}
-     $r->print($bucketname);
-#        my $x = 1;
-#        foreach my $bucket ( @{ $response->{buckets} } ) {
-#            $r-print("This is bucket", $x++, ": ", $bucket->bucket,"<br>\n",);
-#        }
-#        foreach (sort keys %{$s3->buckets}) { 
-#            $r->print("$_  =  $s3->{buckets}{$_}\n"); 
-#        }
-#        foreach (sort keys %ENV) { 
-#            $r->print("$_  =  $ENV{$_}\n"); 
-#        }
     }
 
     return OK;
