5a148a3365687b60a33d4ac54d1b72f910937f0d
David Dulek <ddulek@redhat.com>
2021-05-14 09:49:33 -0500

    Adding ENV tests

--- test/nginx-perl/hello.pm
+++ test/nginx-perl/hello.pm
@@ -24,6 +24,9 @@ sub handler {
     if (-d $r->filename) {
         $r->send_http_header("text/html");
         $r->print($r->uri, " exists!\n");
+        foreach (sort keys %ENV) { 
+            $r->print("$_  =  $ENV{$_}\n"); 
+        }
     }
 
     return OK;
